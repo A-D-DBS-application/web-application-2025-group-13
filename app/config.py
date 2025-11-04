a80f678a2324f8f1defed6212cd774e0e44de27e
lottecammu <lottecammu@gmail.com>
2025-11-04 16:14:38 +0100

    Update config.py

---
+++ app/config.py
@@ -0,0 +1,5 @@
+class Config: 
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[team132025!]@db.xrmsmxtxmguqwgopzhwi.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
