97de6c3e003c181174f48c0eb423a68d4632a95e
Thibaultledegen <thibault.ledegen@ugent.be>
2025-11-05 16:19:17 +0100

    Update app.py

--- app/config.py
+++
@@ -1,5 +0,0 @@
-class Config: 
-    SECRET_KEY = 'team132025!'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[team132025!]@db.xrmsmxtxmguqwgopzhwi.supabase.co:5432/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
