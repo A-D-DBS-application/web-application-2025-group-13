0c3ea2826a6bdb58d2b0d49f85c76ae2369c5b92
Thibaultledegen <thibault.ledegen@ugent.be>
2025-11-05 16:00:11 +0100

    ,

--- app/app.py
+++ app/app.py
@@ -1,4 +1,28 @@
 from app.routes import app
 
+
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/intake')
+def intake():
+    return render_template('intake.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
