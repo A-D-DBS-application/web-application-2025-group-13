f959962821c2012df9e058ed95e571d66121cc91
jolanvantroyen <jolan.vantroyen@ugent.be>
2025-11-06 17:53:25 +0100

    De routes staan in route nu

--- app/app.py
+++ app/app.py
@@ -1,25 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask
+from config import Config
+from routes import main  
 
 app = Flask(__name__)
-@app.route('/register')
-def register():
-    return render_template('register.html')
+app.config.from_object(Config)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/intake')
-def intake():
-    return render_template('intake.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
+app.register_blueprint(main)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session
 
-app = Flask(__name__)
-@app.route('/register')
-def register():
-    return render_template('register.html')
 
-@app.route('/')
+main = Blueprint('main', __name__)
+
+@main.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/about')
+@main.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/intake')
-def intake():
-    return render_template('intake.html')
+@main.route('/register')
+def register():
+    return render_template('register.html')
 
-@app.route('/login')
+@main.route('/login')
 def login():
     return render_template('login.html')
+
+@main.route('/intake')
+def intake():
+    return render_template('intake.html')
