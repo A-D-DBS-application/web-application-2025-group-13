c93d84407299cac0361fd13709b84595e4836fbf
Thibaultledegen <thibault.ledegen@ugent.be>
2025-11-09 16:35:00 +0100

    supabase test, werkt nog niet

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class Config: 
     SECRET_KEY = 'team132025!'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[team132025!]@db.xrmsmxtxmguqwgopzhwi.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:team132025!@db.xrmsmxtxmguqwgopzhwi.supabase.co:5432/postgres?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
